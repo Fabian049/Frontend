b0d49cf2db449049080f8874714b52da5ea11d33
fabian <fabiancalderon049@gmail.com>
2022-10-31 14:32:06 -0500

    update7

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ Pronosticos = [{
     "Partido": "Barcelona Vs Real Madrid", "Pronostico": "+3.5 goles totales", "cuota": 1.45
 }, { "Partido": "Colombia Vs Alemania", "Pronostico": "+3.5 tarjetas totales", "cuota": 1.82 }, { "Partido": "Betis Vs Chelsea", "Pronostico": "+1.5 tiros a puerta por parte de Salah", "cuota": 2.32 }, { "Partido": "Nacional Vs Millonarios", "Pronostico": "+1.5 goles totales", "cuota": 1.40 }, { "Partido": "Palmeiras Vs botafogo", "Pronostico": "-2.5 goles totales", "cuota": 1.65 }, { "Partido": "Bucaramanga Vs Jaguares", "Pronostico": "Dayro Moreno marcara", "cuota": 2.0 }, { "Partido": "Palmeiras Vs botafogo", "Pronostico": "-2.5 goles totales", "cuota": 1.65 }, { "Partido": "Cali Vs Medellin", "Pronostico": "+0.5 tarjetas por parte de Medellin", "cuota": 1.40 }, { "Partido": "Junior Vs Millonarios", "Pronostico": "Gana Junior", "cuota": 1.80 }, { "Partido": "Cucuta Vs Cali", "Pronostico": "Gana Cali", "cuota": 1.60 }]
 
-
+PronosticosAyer = [{
+  "Partido": "Barcelona Vs Real Madrid", "Pronostico": "+3.5 goles totales", "cuota": 1.45,"resultado":"win"
+}, { "Partido": "Colombia Vs Alemania", "Pronostico": "+3.5 tarjetas totales", "cuota": 1.82,"resultado":"win" }, { "Partido": "Betis Vs Chelsea", "Pronostico": "+1.5 tiros a puerta por parte de Salah", "cuota": 2.32,"resultado":"win" }, { "Partido": "Nacional Vs Millonarios", "Pronostico": "+1.5 goles totales", "cuota": 1.40,"resultado":"win" }, { "Partido": "Palmeiras Vs botafogo", "Pronostico": "-2.5 goles totales", "cuota": 1.65,"resultado":"win" }, { "Partido": "Bucaramanga Vs Jaguares", "Pronostico": "Dayro Moreno marcara", "cuota": 2.0,"resultado":"win" }, { "Partido": "Palmeiras Vs botafogo", "Pronostico": "-2.5 goles totales", "cuota": 1.65,"resultado":"win" }, { "Partido": "Cali Vs Medellin", "Pronostico": "+0.5 tarjetas por parte de Medellin", "cuota": 1.40,"resultado":"win" }, { "Partido": "Junior Vs Millonarios", "Pronostico": "Gana Junior", "cuota": 1.80,"resultado":"win" }, { "Partido": "Cucuta Vs Cali", "Pronostico": "Gana Cali", "cuota": 1.60,"resultado":"win" }]
 
 const listaCuotas = document.getElementById('body');
 const listaCuotasSmooth = document.getElementById('responsive-table');
@@ -35,4 +37,25 @@ listaCuotasSmooth.innerHTML += `
     );
 
 }
+
+
+const listaCuotasAyer = document.getElementById('bodyAyer');
+const listaCuotasSmoothAyer = document.getElementById('responsive-tableAyer');
+function CrearListAyer() {
+  PronosticosAyer.map((cuotas, index)=>{
+      listaCuotasAyer.innerHTML += `        
+        <tr id="${index+1}">
+      <td>${index+1}</td>
+      <td>${cuotas.Partido}</td>
+      <td>${cuotas.Pronostico}</td>
+      <td>${cuotas.cuota}</td>
+      <td>${cuotas.resultado}</td>
+    </tr> `
+  })
+
+
+
+}
 CrearLista();
+CrearListAyer();
+
